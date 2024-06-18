d31853a819fbfada6ad7c4e41acf548d6b8ce02d
Artem V. Tretiak <artem.tretiak@stuzo.com>
2024-06-18 18:30:23 +0300

    Migrate to aws sdk v3

--- src/aws.js
+++ src/aws.js
@@ -1,4 +1,4 @@
-const AWS = require('aws-sdk');
+const { EC2, waitUntilInstanceRunning } = require('@aws-sdk/client-ec2');
 const core = require('@actions/core');
 const config = require('./config');
 
@@ -46,7 +46,7 @@ function buildMarketOptions() {
 }
 
 async function startEc2Instance(label, githubRegistrationToken) {
-  const ec2 = new AWS.EC2();
+  const ec2 = new EC2();
 
   const userData = buildUserDataScript(githubRegistrationToken, label);
 
@@ -64,7 +64,7 @@ async function startEc2Instance(label, githubRegistrationToken) {
   };
 
   try {
-    const result = await ec2.runInstances(params).promise();
+    const result = await ec2.runInstances(params);
     const ec2InstanceId = result.Instances[0].InstanceId;
     core.info(`AWS EC2 instance ${ec2InstanceId} is started`);
     return ec2InstanceId;
@@ -75,14 +75,14 @@ async function startEc2Instance(label, githubRegistrationToken) {
 }
 
 async function terminateEc2Instance() {
-  const ec2 = new AWS.EC2();
+  const ec2 = new EC2();
 
   const params = {
     InstanceIds: [config.input.ec2InstanceId],
   };
 
   try {
-    await ec2.terminateInstances(params).promise();
+    await ec2.terminateInstances(params);
     core.info(`AWS EC2 instance ${config.input.ec2InstanceId} is terminated`);
     return;
   } catch (error) {
@@ -92,14 +92,17 @@ async function terminateEc2Instance() {
 }
 
 async function waitForInstanceRunning(ec2InstanceId) {
-  const ec2 = new AWS.EC2();
+  const ec2 = new EC2();
 
   const params = {
     InstanceIds: [ec2InstanceId],
   };
 
   try {
-    await ec2.waitFor('instanceRunning', params).promise();
+    await waitUntilInstanceRunning({
+      client: ec2,
+      maxWaitTime: 200,
+    }, params);
     core.info(`AWS EC2 instance ${ec2InstanceId} is up and running`);
     return;
   } catch (error) {
